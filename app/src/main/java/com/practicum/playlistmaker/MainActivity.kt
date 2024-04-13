01d04eda8402069e701c5eae94afb964043f358a
cyberschoolinmoscow <cyberschool.moscow@gmail.com>
2024-04-13 18:02:17 +0300

    Добавлены layout\activity_main, иконки для кнопок

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,14 @@ package com.practicum.playlistmaker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myToolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myToolbar = findViewById(R.id.myToolbar)
+        setSupportActionBar(myToolbar)
     }
 }
