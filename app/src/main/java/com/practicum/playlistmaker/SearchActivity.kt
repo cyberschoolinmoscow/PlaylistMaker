ef96c0f7756fe063b85cd675beb776cdc72ee8cf
cyberschoolinmoscow <cyberschool.moscow@gmail.com>
2024-06-04 12:17:58 +0300

    Удалены неиспользуемые строки кода

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -84,81 +84,66 @@ class SearchActivity : AppCompatActivity() {
 
         val recycler = findViewById<RecyclerView>(R.id.trackList)
         recycler.layoutManager = LinearLayoutManager(this)
-//        recycler.adapter =if(isOnline(this))  {TrackAdapter(tracks = getTracks())  }else{null}
-
-//        trackAdapter = TrackAdapter(tracks = tracks)
-
         recycler.adapter = trackAdapter
 
         inputEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
-                if (inputEditText.text.isNotEmpty()) {
-                    searchRequest(inputEditText.text.toString())
-                }
+                searchRequest()
                 true
             }
             false
         }
         buttonUpdate.setOnClickListener {
-            if (inputEditText.text.isNotEmpty()) {
-                searchRequest(inputEditText.text.toString())
-            }
+            searchRequest()
         }
     }
 
-    private fun searchRequest(text: String) {
+    private fun searchRequest() {
         placeholderLayout.isVisible = false
-        imdbService.findTrack(text).enqueue(object :
-            Callback<TrackResponse> {
+        if (inputEditText.text.isNotEmpty()) {
+            imdbService.findTrack(inputEditText.text.toString()).enqueue(object :
+                Callback<TrackResponse> {
 
-            override fun onResponse(
-                call: Call<TrackResponse>,
-                response: Response<TrackResponse>
-            ) {
-                if (response.code() == 200) {
-                    tracks.clear()
-                    if (response.body()?.results?.isNotEmpty() == true) {
-                        tracks.addAll(response.body()?.results!!)
-                        trackAdapter.notifyDataSetChanged()
-                    } else if (tracks.isEmpty()) {
-//                                    val internet: Int = R.drawable.internet
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (response.code() == 200) {
+                        tracks.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            tracks.addAll(response.body()?.results!!)
+                            trackAdapter.notifyDataSetChanged()
+                        } else if (tracks.isEmpty()) {
+                            showMessage(
+                                getString(R.string.nothing_found),
+                                R.drawable.nothing,
+                                false
+                            )
+                        }
+                    } else {
                         showMessage(
-                            getString(R.string.nothing_found),
-                            "",
-                            R.drawable.nothing,
-                            false
+                            getString(R.string.something_went_wrong),
+                            R.drawable.internet,
+                            true
                         )
                     }
-//                                else {
-//                                    showMessage("", "", 0, false)
-//                                }
-                } else {
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
                     showMessage(
                         getString(R.string.something_went_wrong),
-                        response.code().toString(),
                         R.drawable.internet,
                         true
                     )
+
                 }
-            }
 
-            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
-                showMessage(
-                    getString(R.string.something_went_wrong),
-                    t.message.toString(),
-                    R.drawable.internet,
-                    true
-                )
-
-            }
-
-        })
+            })
+        }
     }
 
     private fun showMessage(
         text: String,
-        additionalMessage: String,
         drawable: Int,
         visibility: Boolean
     ) {
@@ -170,10 +155,6 @@ class SearchActivity : AppCompatActivity() {
             placeholderMessage.text = text
 
             placeholderMessage.setCompoundDrawablesWithIntrinsicBounds(0, drawable, 0, 0)
-//            if (additionalMessage.isNotEmpty()) {
-//                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-//                    .show()
-//            }
         } else {
             placeholderMessage.isVisible = false
         }
@@ -200,41 +181,6 @@ class SearchActivity : AppCompatActivity() {
         }
         return false
     }
-
-    private fun getTracks() = listOf<Track>(
-
-        Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),
-
-        Track(
-            "Billie Jean",
-            "Michael Jackson",
-            "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ),
-
-        Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Whole Lotta Love",
-            "Led Zeppelin", "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-        )
-    )
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
